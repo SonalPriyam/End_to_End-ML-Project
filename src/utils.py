a183144fc4d7ff4778d82f7770793cf8220019fc
Sonal Priyam <sonalpriyam39@gmail.com>
2025-06-17 10:47:05 +0530

    trained model

--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,16 @@
 import os
 import sys
+from unittest import mock
 
-import numpy as np
+sys.path.insert(0, r"C:\Users\mvuyiso.gqwaru\OneDrive - Wabtec Corporation\Documents\MLOps\Krish Naik - End to End")
+
 import pandas as pd
-import dill
-
+import numpy as np
 from src.exception import CustomException
+from src.logger import logging
+import dill
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+from sklearn.model_selection import GridSearchCV
 
 
 def save_object(file_path, obj):
@@ -19,3 +24,38 @@ def save_object(file_path, obj):
 
     except Exception as e:
         raise CustomException(e, sys)
+    
+    logging.info("A file was saved")
+
+
+def evaluate_model(X_train, y_train, X_test, 
+                   y_test, models, params):
+    try:
+
+        report={}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para = params[list(models.keys())[i]]
+
+            gs =  GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            #mae = mean_absolute_error(y_train, y_train_pred)
+            #mse = mean_squared_error(y_train, y_train_pred)
+            #rmse = np.sqrt(mean_squared_error(y_train, y_train_pred)
+                
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = (test_model_score)
+
+        return report
+    
+    except Exception as e:
+        raise CustomException(e,sys)
